760bac21dc93b53cee24e4f583b5f662c3937fc3
Steve Holland <sdh4@iastate.edu>
2024-08-07 13:21:08 -0500

    Verify correct operation and add push-to-pypi workflow

--- README.md
+++ README.md
@@ -5,16 +5,25 @@ dgpython-local is a placeholder package that is intended so you
 can make your own private branches with dataguzzler-python
 configuration local to your organization, laboratory, or system.
 
-Be sure to set local_identifier in setup.py to something that
-clearly identifies your organization or laboratory. It may
-contain ASCII characters a-z and A-Z and digits 0-9 and
+In most cases you don't want to install the published version,
+except possibly as a placeholder until you have your own
+customizations in place. Instead, make your customizations
+to dgpython_local/local.dpi that define equipment installed
+locally, and then install your customized version.
+
+Be sure to set local_identifier in setup.py to
+something that clearly identifies your organization or laboratory.
+It may contain ASCII characters a-z and A-Z and digits 0-9 and
 periods.
 
 The example dgpython_local/local.dpi illustrates how you can
-provide local functionality when available that might vary
-between computers. 
+provide local functionality. Since it is all Python code,
+you can perform operations such as checking the hostname
+to select appropriate equipment, or you can just keep
+different customized versions for different computers.
 
-Basic requirements are Python v3.8 or above with the following packages: numpy, setuptools, wheel, build, setuptools_scm
+Basic requirements are Python v3.8 or above with the following
+packages: numpy, setuptools, wheel, build, setuptools_scm, pip
 
 Basic installation is (possibly as root or Administrator):
     pip install --no-deps --no-build-isolation .
--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import sys
 
 from setuptools import setup, Extension
+from setuptools_scm import get_version
 import os
 import os.path
 import re
@@ -15,12 +16,15 @@ except ModuleNotFoundError:
     from distutils.command.build import build
     pass
 
-#Be sure to set local_identifier to something that
-#clearly identifies your organization or laboratory. It may
-#contain ASCII characters a-z and A-Z and digits 0-9 and
-#periods.
+# Be sure to set local_identifier
+# to something that clearly identifies your organization
+# or laboratory. It may contain ASCII characters a-z and
+# A-Z and digits 0-9 and periods.
+
 local_identifier=None
-local_identifier="thermal.cnde.iastate.edu"
+#local_identifier="thermal.cnde.iastate.edu"
+
+
 
 ext_modules = []
 package_data = {
@@ -38,6 +42,8 @@ console_scripts_entrypoints = [ "%s = dgpython_local.bin.%s:main" % (script,scri
 
 
 def local_version_scheme(version): #version is a setuptools_scm.ScmVersion object.
+    #print("Local_Version_Scheme")
+    time_format="%Y%m%d"
     if version.exact or version.node is None:
         return version.format_choice(
             "", "+d{time:{time_format}}", time_format=time_format
@@ -53,22 +59,34 @@ def local_version_scheme(version): #version is a setuptools_scm.ScmVersion objec
             )
         pass
     pass
+
+if __name__ == "__main__":
     
         
-setup(name="dgpython_local",
-      description="local module definitions for dataguzzler python",
-      author="Stephen D. Holland",
-      #version=version,
-      url="http://thermal.cnde.iastate.edu",
-      ext_modules=[],
-      zip_safe=False,
-      cmdclass={
+    setup(name="dgpython_local",
+          description="local module definitions for dataguzzler python",
+          author="Stephen D. Holland",
+          #version=version,
+          url="http://thermal.cnde.iastate.edu",
+          ext_modules=[],
+          zip_safe=False,
+          cmdclass={
         
-      },
-      packages=[
-          "dgpython_local",
-      #    "dgpython_local.bin",
-      ],
-      package_data=package_data,
-      entry_points={"console_scripts": console_scripts_entrypoints },
-      use_scm_version={"local_scheme": local_version_scheme })
+          },
+          packages=[
+              "dgpython_local",
+              #    "dgpython_local.bin",
+          ],
+          package_data=package_data,
+          entry_points={"console_scripts": console_scripts_entrypoints },
+          # NOTE: We are using get_version() here because with current
+          # setuptools_scm (8.0.4) we don't consistently get our local
+          # version scheme with the local_identifier to be used.
+          # The following must all work:
+          #     python -m setuptools_scm
+          #     python -m build .   # (perhaps with --no-isolation)
+          #     python -m pip install .  # (perhaps with --no-build-isolation) 
+          version=get_version(local_scheme=local_version_scheme),
+          use_scm_version={"local_scheme": local_version_scheme }
+          )
+    pass 
